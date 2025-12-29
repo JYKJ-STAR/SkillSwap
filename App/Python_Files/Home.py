5348a50cca2a2e4925e472a4efe4c161f54076f3
JYKJ-STAR <jaydenyipkj@gmail.com>
2025-12-29 23:42:40 +0800

    File changes

--- App/Python_Files/Home.py
+++ App/Python_Files/Home.py
@@ -6,6 +6,6 @@ home_bp = Blueprint("home", __name__)
 @home_bp.route("/")
 def home():
     conn = get_db_connection()
-    user_count = conn.execute('SELECT COUNT(*) FROM users').fetchone()[0]
+    user_count = conn.execute('SELECT COUNT(*) FROM user').fetchone()[0]
     conn.close()
     return f"<h1>SkillSwap Application</h1><p>Welcome! SQLite Connection Successful.</p>"
